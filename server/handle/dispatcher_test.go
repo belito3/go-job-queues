f11c75bebb950d9c1d470c811a7d7c093b659aab
baobv <45716099+belito3@users.noreply.github.com>
2024-06-10 15:08:47 +0700

    update dispatcher_test.go

--- server/handle/dispatcher_test.go
+++ server/handle/dispatcher_test.go
@@ -1,13 +1,14 @@
 package handle
 
 import (
+	"os"
 	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
 )
 
-func TestMain(t *testing.T) {
+func newTestServer(t *testing.T) {
 	var sent, processed uint64
 	wg := sync.WaitGroup{}
 
@@ -16,12 +17,13 @@ func TestMain(t *testing.T) {
 		wg.Done()
 		return nil
 	}
+	print(process)
 
-	dispatcher := NewDispatcher(2, 10, process)
+	dispatcher := NewDispatcher(2, 10)
 	dispatcher.Run()
 
 	spam := func() {
-		for i := 0; i < 5; i ++ {
+		for i := 0; i < 5; i++ {
 			atomic.AddUint64(&sent, 1)
 
 			work := Job{Payload{"1"}}
@@ -41,4 +43,10 @@ func TestMain(t *testing.T) {
 		t.Errorf("not all jobs were processed %v jobs %v processed",
 			sent, processed)
 	}
+}
+
+func TestMain(m *testing.M) {
+	var t *testing.T
+	newTestServer(t)
+	os.Exit(m.Run())
 }
